6682fce36cf158eaee58abcad3f823ce434aa401
PhileasDupont <81762192+PhileasDupont@users.noreply.github.com>
2021-10-05 18:24:19 +0200

    Add files via upload

---
+++ project_1/numbers_dict.py
@@ -0,0 +1,34 @@
+
+numbers_dict={}
+numbers_dict[10] = 'A'
+numbers_dict[11] = 'B'
+numbers_dict[12] = 'C'
+numbers_dict[13] = 'D'
+numbers_dict[14] = 'E'
+numbers_dict[15] = 'F'
+numbers_dict[16] = 'G'
+numbers_dict[17] = 'H'
+numbers_dict[18] = 'I'
+numbers_dict[19] = 'J'
+numbers_dict[20] = 'K'
+numbers_dict[21] = 'L'
+numbers_dict[22] = 'M'
+numbers_dict[23] = 'N'
+numbers_dict[24] = 'O'
+numbers_dict[25] = 'P'
+numbers_dict[26] = 'Q'
+numbers_dict[27] = 'R'
+numbers_dict[28] = 'S'
+numbers_dict[29] = 'T'
+numbers_dict[30] = 'U'
+numbers_dict[31] = 'V'
+numbers_dict[32] = 'W'
+numbers_dict[33] = 'X'
+numbers_dict[34] = 'Y'
+numbers_dict[35] = 'Z'
+
+
+
+
+
+
---
+++ project_1/projet1page25.py
@@ -0,0 +1,145 @@
+import time
+
+from numbers_dict import *
+
+#classe des couleurs
+class basics_colors:
+    OK = '\033[92m' #GREEN
+    WARNING = '\033[93m' #YELLOW
+    FAIL = '\033[91m' #RED
+    RESET = '\033[0m' #RESET COLOR
+
+#exercice 1 
+def decimale_binaire(n):
+    req_value_1 = input("donnez un chiffre en base 10 \n >>>")
+    n = int(req_value_1)
+
+    req_binary(n)
+    #end decimale_binaire
+
+def req_binary(n):
+    vInit = n
+    binary_list = []
+    while n != 0:
+        varList = n % 2
+        binary_list.insert(0, varList)
+        n = n // 2
+
+    if n == 0:
+        resList = ' ,'.join(map(str,binary_list))
+        resListTotal = ''.join(map(str,binary_list)) 
+        print(basics_colors.OK, vInit, " en base 10 équivaut à ", resList, " ( ", resListTotal, " ) en base 2", basics_colors.RESET)
+        time.sleep(3)
+        requestType()
+
+# exercice 2
+def decimale_hexadecimale(n):
+    n = 0
+    req_value_1 = input("donnez un chiffre en base 10 \n >>>")
+    n = int(req_value_1)
+    reqBase = 16
+
+    req_hexa(n, reqBase)
+    # end decimale_hexadecimale
+
+def req_hexa(n, reqBase):
+    
+    varListStr = 0
+    vInit = n
+    binary_list = []
+    while n != 0:
+        varList = n % reqBase
+        n = n // reqBase
+
+        if varList > 9:
+            binary_list.insert(0, numbers_dict[varList])
+        if varList <= 9:
+            binary_list.insert(0, varList)
+            
+        if n == 0:
+            resList = ' ,'.join(map(str,binary_list))
+            resListTotal = ''.join(map(str,binary_list)) 
+            print(basics_colors.OK, vInit, " en base 10 équivaut à ", resList, " ( ", resListTotal, " ) en base ", reqBase, basics_colors.RESET)
+            time.sleep(3)
+            requestType()
+    
+# exercice 3
+def base_request(n):
+    n = 0
+    req_value_1 = input("donnez un chiffre en base 10 \n >>> ")
+    vInit = int(req_value_1)
+    n = int(req_value_1)
+
+    if n == 0:
+        print(basics_colors.OK, "0 vaut en 0 en toutes les bases ", basics_colors.RESET)
+        time.sleep(1)
+        requestType()
+    if n < 0:
+        print(f"{basics_colors.WARNING}ERREUR : le nombre est négatif, réesayez{basics_colors.RESET}")
+        time.sleep(1)
+        requestType()
+    
+    else:
+        print(f"{basics_colors.FAIL}ERREUR : une erreur est survenue, merci de réessayer{basics_colors.RESET}")
+        time.sleep(1)
+        requestType()
+
+    req_value_2 = input("donnez la base en laquelle vous voulez convertir  \n >>>")
+    reqBase = int(req_value_2)
+
+    if reqBase <= 0:
+        print(f"{basics_colors.WARNING}ERREUR : le nombre est négatif ou égal à 0, réesayez{basics_colors.RESET}")
+        time.sleep(1)
+        requestType()
+    
+    binary_list = []
+
+    if reqBase == 2: #renvoie vers l'exercice de base 2
+        req_binary(n)
+        requestType()
+    if reqBase > 9:
+        req_hexa(n, reqBase) #renvoie vers l'exercice de base 16
+        requestType()
+
+    while n != 0:
+        varList = n % reqBase
+        binary_list.insert(0, varList)
+        n = n // reqBase
+
+    if n == 0:
+        resList = ' ,'.join(map(str,binary_list))
+        resListTotal = ''.join(map(str,binary_list)) 
+        print(basics_colors.OK, vInit, " en base 10 équivaut à ", resList, " ( ", resListTotal, " ) en base ", reqBase, basics_colors.RESET)
+        time.sleep(3)
+        requestType()
+    
+    else:
+        print(f"{basics_colors.FAIL}ERREUR : une erreur est survenue, merci de réessayer{basics_colors.RESET}")
+        time.sleep(1)
+        requestType()
+
+
+# demande ce que l'user veut faire 
+def requestType():
+    request = input("que voulez vous faire ? \n '1' -> conersion décimale vers binaire \n '2' -> conversion décimale vers hexadécimale \n '3' -> conversion décimale vers base demandée \n >>>")
+    n = int(request)
+    if n == 1: 
+        decimale_binaire(n) # -> Exercice 1 -> partie du calcul
+        return()
+    if n == 2:
+        decimale_hexadecimale(n) # -> Exercice 2 -> partie du calcul
+        return()
+    if n == 3:
+        base_request(n) # -> Exercice 1 -> partie de demande puis de calcul
+        return()
+    else: 
+        print(f"{basics_colors.WARNING}ERREUR : l'entrée est incorrecte, réesayez{basics_colors.RESET}")
+        time.sleep(2)
+        requestType()  
+        return() 
+
+#debut du code
+requestType()
+
+
+# fonction pour les nombres négatifs (donner une soltion)
