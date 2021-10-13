63a091e1c25460a03c4396194f1b618caeb0ae11
PhileasDupont <81762192+PhileasDupont@users.noreply.github.com>
2021-10-13 20:28:13 +0200

    v 4.0

--- project_1/projet1page25.py
+++ project_1/projet1page25.py
@@ -1,9 +1,10 @@
 # Projet 1 page 25
-# Version 3.2.1
+# Version 4.0.1
 # Version en cours de documentation
 
 import time
 from numbers_dict import *
+import webbrowser
 
 #classe des couleurs
 class basics_colors:
@@ -15,13 +16,24 @@ def error_message(args): # envoyé avec args, envoie un message d'erreur
     print(basics_colors.FAIL, errors_dict[args] ,basics_colors.RESET)
     time.sleep(1)
     requestType()
-    
+
+def verif(request):
+    if request == 'q':
+        print(basics_colors.OK, ' retour au début ...', basics_colors.RESET)
+        time.sleep(0.5)
+        requestType()
+    if request == 'help' or request == 'h':
+        print(basics_colors.OK,"redirection sur le GitHub ...", basics_colors.RESET)
+        webbrowser.open('https://github.com/PhileasDupont/NSI/blob/main/project_1/readme.txt')
+        time.sleep(2)
+        requestType()
+
 #exercice 1 
 def decimale_binaire(n): # envoyé grace à requestType(), renvoie la fonction de demande de calcul pour la base 2
-    req_value_1 = input("donnez un chiffre en base 10 \n >>>")
-
-    if (req_value_1.isdigit()):
-        n = int(req_value_1)
+    request = input("donnez un chiffre en base 10 \n >>>")
+    verif(request)
+    if (request.isdigit()):
+        n = int(request)
         if n == 0 or n == 1: # valeurs par defaut
             print(basics_colors.OK, n, " vaut ", n," en toutes les bases ", basics_colors.RESET)
         if n < 0: # erreur
@@ -51,10 +63,10 @@ def req_binary(n): # envoyé par requestType(), prend en fonction le nombre n de
 # exercice 2
 def decimale_hexadecimale(n): # envoyé grace à requestType() ou req_binary(), renvoie la fonction de demande de calcul pour la base 16
     n = 0
-    req_value_1 = input("donnez un chiffre en base 10 \n >>>")
-
-    if ( req_value_1.isdigit()):
-        n = int(req_value_1)
+    request = input("donnez un chiffre en base 10 \n >>>")
+    verif(request)
+    if ( request.isdigit()):
+        n = int(request)
 
         if n == 0 or n == 1: # valeurs par défaut
             print(basics_colors.OK, n, " vaut ", n," en toutes les bases ", basics_colors.RESET)
@@ -80,11 +92,8 @@ def req_hexa(n, reqBase): # envoyé par decimale_hexadecimal(), prend en compte 
         varList = n % reqBase
         n = n // reqBase
 
-        if varList > 9: # si c'est un chiffre (0 -> 9 )
-            binary_list.insert(0, numbers_dict[varList])
-        if varList <= 9: # si supérieur à 9 renvoie les lettres
-            binary_list.insert(0, varList)
-            
+        binary_list.insert(0, alpha[varList])
+
         if n == 0:
             resList = ' ,'.join(map(str,binary_list)) # faire une liste visible
             resListTotal = ''.join(map(str,binary_list)) # faire une liste visible
@@ -96,11 +105,11 @@ def req_hexa(n, reqBase): # envoyé par decimale_hexadecimal(), prend en compte 
 # exercice 3
 def base_request(n):
     n = 0
-    req_value_1 = input("donnez un chiffre en base 10 \n >>> ")
-
-    if ( req_value_1.isdigit()):
-        vInit = int(req_value_1)
-        n = int(req_value_1)
+    request = input("donnez un chiffre en base 10 \n >>> ")
+    verif(request)
+    if ( request.isdigit()):
+        vInit = int(request)
+        n = int(request)
 
         if n < 0: # erreur
             args = 2
@@ -110,20 +119,19 @@ def base_request(n):
             time.sleep(1)
             requestType()
 
-        req_value_2 = input("donnez la base en laquelle vous voulez convertir  \n >>>")
-
-        if ( req_value_2.isdigit()):
-            reqBase = int(req_value_2)
+        request = input("donnez la base en laquelle vous voulez convertir  \n >>>")
+        verif(request)
+        if ( request.isdigit()):
+            reqBase = int(request)
 
             if reqBase <= 1: # erreur
                 args = 3
                 error_message(args)
-            if reqBase > 36: # erreur
+            
+            if reqBase > 62:
                 args = 5
                 error_message(args)
-
             binary_list = []
-
             if reqBase == 2: #renvoie vers l'exercice de base 2
                 req_binary(n)
                 requestType()
@@ -140,8 +148,11 @@ def base_request(n):
 
 # demande ce que l'user veut faire 
 def requestType():
-    request = input("que voulez vous faire ? \n '1' -> conersion décimale vers binaire \n '2' -> conversion décimale vers hexadécimale \n '3' -> conversion décimale vers base demandée \n >>>")
-    
+    print(basics_colors.OK, "debut du programme ...", basics_colors.RESET)
+    time.sleep(1)
+    print("que voulez vous faire ? \n '1' -> conersion décimale vers binaire \n '2' -> conversion décimale vers hexadécimale \n '3' -> conversion décimale vers base demandée \n A n'importe quel moment tapez ", basics_colors.FAIL, ' q ', basics_colors.RESET, " pour ", basics_colors.FAIL, "RECOMMENCER", basics_colors.RESET, " ou tapez ", basics_colors.FAIL, " help ", basics_colors.RESET, " pour être ", basics_colors.FAIL, " redirigé vers la page d'aide", basics_colors.RESET)
+    request = input(">>>")
+    verif(request)
     if ( request.isdigit()):
         n = int(request)
         if n > 3 or n <= 0:
